631adc64c08f333e3292370f4d483f75a6ba29f9
Ulunji Felix Mkandawire <designedbyulunji@gmail.com>
2024-01-25 12:03:25 +0200

    added bottom button and open hours indicator

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,23 @@
 "use strict"
+
+/* HERO SECTION */
+
+const navIconEl = document.querySelector(".hero-section__nav-icon");
+const navLinksEl = document.querySelector(".hero-section__nav-links");
+
+navIconEl.addEventListener("click", switchIcon);
+
+
+function switchIcon() {
+  navIconEl.classList.toggle("nav--icon");
+
+  if (navIconEl.classList.contains("nav--icon")) {
+    navLinksEl.style.transform = `translateX(0)`;
+  }
+
+  else {
+    navLinksEl.style.transform = `translateX(1000px)`;
+
+  }
+
+}
